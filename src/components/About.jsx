cb04f78d3c9ba7323f63760d00f4f0a04f8f709c
Jesus Valadez <seaofjosus@gmail.com>
2026-02-01 21:34:38 -0600

    about section changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,12 @@
 export default function About() {
     return (
         <>
-            <section id={"about"} className="about">
+            <section id="about" className="about">
                 <h1>About</h1>
-
+                <div className="about-container">
+                    <div className="about-item-one"></div>
+                    <div className="about-item-two"></div>
+                </div>
             </section>
         </>
     )
