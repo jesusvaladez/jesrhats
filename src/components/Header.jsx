baad763f9ac3fbd89a4c1cd4fbd4c9a9986e9d6f
Jesus Valadez <seaofjosus@gmail.com>
2026-02-07 23:24:56 -0600

    working on responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ export default function Header() {
         <header className="header">
             <nav className="top-navbar">
                 <ul className="navbar-bp">
-                    <div className="left-container">
-                        <li><a href>JesrhaTs</a></li>
-                    </div>
+                    <li className="left-container">
+                        <a href>JesrhaTs</a>
+                    </li>
 
-                    <div className="right-container">
-                        <li><a href={"#home"} className={"right-bp"}>Home</a></li>
-                        <li><a href={"#products"} className={"right-bp"}>Products</a></li>
-                        <li><a href={"#about"} className={"right-bp"}>About</a></li>
-                        <li><a href={"#contact"} className={"right-bp"}>Contact</a></li>
-                    </div>
+                    <li className="right-container">
+                        <a href={"#home"} className={"right-bp"}>Home</a>
+                        <a href={"#products"} className={"right-bp"}>Products</a>
+                        <a href={"#about"} className={"right-bp"}>About</a>
+                        <a href={"#contact"} className={"right-bp"}>Contact</a>
+                    </li>
                 </ul>
             </nav>
         </header>
