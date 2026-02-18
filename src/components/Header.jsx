06804d108d5c032955e2f622d395fc1c66dc8688
Jesus Valadez <seaofjosus@gmail.com>
2026-02-17 21:04:56 -0600

    Partial hamburger menu impl

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,28 @@
+import {useState} from "react";
+
+
+
 export default function Header() {
+    const [open, setOpen] = useState(false);
+
     return (
         <header className="header">
             <nav className="top-navbar">
-                <ul className="navbar-bp">
+
+                <button className="hamburger" onClick={() => setOpen(!open)}>
+                    ☰
+                </button>
+
+                <ul className={`navbar-bp ${open ? "active" : " "}`}>
                     <li className="left-container">
                         <a href>JesrhaTs</a>
                     </li>
 
                     <li className="right-container">
-                        <a href={"#home"} className={"right-bp"}>Home</a>
-                        <a href={"#products"} className={"right-bp"}>Products</a>
-                        <a href={"#about"} className={"right-bp"}>About</a>
-                        <a href={"#contact"} className={"right-bp"}>Contact</a>
+                        <a href={"#home"} className={"right-bp"} onClick={() => setOpen(false)} >Home</a>
+                        <a href={"#products"} className={"right-bp"} onClick={() => setOpen(false)} >Products</a>
+                        <a href={"#about"} className={"right-bp"} onClick={() => setOpen(false)} >About</a>
+                        <a href={"#contact"} className={"right-bp"} onClick={() => setOpen(false)} >Contact</a>
                     </li>
                 </ul>
             </nav>
