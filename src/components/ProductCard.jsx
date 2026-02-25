c10c9266384b8b2fe19478fc2f0aeec4734e2151
Jesus Valadez <seaofjosus@gmail.com>
2026-02-25 16:39:22 -0600

    styling impls to product section

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -7,6 +7,7 @@ export default function ProductCard( { product } ) {
                     <img src={product.img.src} alt={product.img.alt} />
                 </div>
                 <div className="product-details">
+                    <span className="product-type">{product.type}</span>
                     <h3 className="product-name">{product.title}</h3>
                     <p className="product-price">{product.price}</p>
                     <button className="add-to-cart">Add To Cart</button>
